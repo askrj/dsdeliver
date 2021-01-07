b14024ec87c692558027967023b62293e9eef1bf
ask.rj10@gmail.com <ask.rj10@gmail.com>
2021-01-07 10:01:15 -0300

    Add Home Page

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,10 +1,11 @@
 import './App.css';
 import Navbar from './Navbar/index';
-
+import Home from './Home/index';
 function App() {
   return (
     <div>
      <Navbar/>
+     <Home/>
     </div>
   );
 }
